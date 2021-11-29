4547d40ab61ccc3349bbdac08891155d5675d4e7
khan2876 <akhan@bpsk12.org>
2021-11-29 13:17:07 -0500

    Updated to include gesture names

--- models/accuracy_by_class.py
+++ models/accuracy_by_class.py
@@ -4,6 +4,7 @@ import numpy as np
 def accuracy(actual, predicted):
     tp = np.zeros(4)
     counts = np.zeros(4)
+    gesture = ["Rock", "Scissors", "Paper", "OK"]
 
     for i in range(len(actual)):
         counts[int(actual[i])] += 1
@@ -11,6 +12,6 @@ def accuracy(actual, predicted):
             tp[int(actual[i])] += 1
 
     for i in range(4):
-        print("Accuracy for " + str(i) + " is " + str(tp[i] / counts[i]))
+        print("Accuracy for " + str(i) + " (" + gesture[i] + ") is %.2f" % (tp[i] / counts[i] * 100))
 
-    print("Overall accuracy is " + str(np.sum(tp) / np.sum(counts)))
+    print("Overall accuracy is %.2f" % (np.sum(tp) / np.sum(counts * 100)))
